d60efcfd22e8e69b33bb4bae324d16e2b5a3403a
ritvik06 <rvij@DESKTOP-1TL12UE.localdomain>
2019-10-04 19:48:21 +0530

    Graphs created from I/O

---
+++ graph_solve.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+class node{
+	private:
+		int indegree=0;
+		int outdegree=0;
+		vector<int> neighbours;
+
+	public:
+		void set_indegree(){
+			indegree++;
+		}
+		int get_indegree(){
+			return indegree;
+		}
+		void set_outdegree(){
+			outdegree++;
+		}
+		int get_outdegree(){
+			return outdegree;
+		}
+		void set_neg(int val){
+			neighbours.push_back(val);
+		}
+		void get_neg(){
+			for(int i=0;i<neighbours.size();i++){
+				cout << neighbours[i] << " ";
+			}
+			cout << endl;
+		}
+};
+
+unordered_map<int,node> Graph;
+unordered_map<int,node> Graph_Dash;
+
+void input(string filename){
+	int c1,c2;
+	ifstream file;
+
+	file.open(filename);
+
+	if(file.is_open()){
+		file >> c1 >> c2;
+		do{
+			if(Graph.find(c1)!=Graph.end()){
+				Graph[c1].set_outdegree();
+				// Graph[c2].indegree++;
+				Graph[c1].set_neg(c2);
+				if(Graph.find(c2)!=Graph.end()){
+					Graph[c2].set_indegree();
+				}
+				else{
+					node new2;
+					new2.set_indegree(); 
+					Graph[c2] = new2;
+				}
+			}
+			else{
+				node new1;
+				new1.set_outdegree();
+				new1.set_neg(c2);
+				if(Graph.find(c2)!=Graph.end()){
+					Graph[c2].set_indegree();
+				}
+				else{
+					node new2;
+					new2.set_indegree(); 
+					Graph[c2] = new2;
+				}	
+				Graph[c1] = new1;
+			}
+		}while(c1!=0 && c2!=0)
+
+		while(file.is_open()){
+			if(Graph_Dash.find(c1)!=Graph_Dash.end()){
+				Graph_Dash[c1].set_outdegree();
+				// Graph[c2].indegree++;
+				Graph_Dash[c1].set_neg(c2);
+				if(Graph_Dash.find(c2)!=Graph_Dash.end()){
+					Graph_Dash[c2].set_indegree();
+				}
+				else{
+					node new2;
+					new2.set_indegree(); 
+					Graph_Dash[c2] = new2;
+				}
+			}
+			else{
+				node new1;
+				new1.set_outdegree();
+				new1.set_neg(c2);
+				if(Graph_Dash.find(c2)!=Graph_Dash.end()){
+					Graph_Dash[c2].set_indegree();
+				}
+				else{
+					node new2;
+					new2.set_indegree(); 
+					Graph_Dash[c2] = new2;
+				}	
+				Graph_Dash[c1] = new1;
+			}			
+		}
+
+	}
+	else{
+		cout << " File doesn't exist" << endl;
+	}
+	
+
+}
+
+int main(){
+	return 0;
+}
