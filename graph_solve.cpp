405450c0a74fe69832f2000043807a71329d6d65
JayParanjape <jay.edutech@gmail.com>
2019-10-06 13:17:00 +0530

    added more clauses

--- graph_solve.cpp
+++ graph_solve.cpp
@@ -12,6 +12,7 @@ class node{
 		int indegree=0;
 		int outdegree=0;
 		vector<int> neighbours;
+		//int count_clauses=0;
 
 	public:
 		void set_indegree(){
@@ -29,21 +30,16 @@ class node{
 		void set_neg(int val){
 			neighbours.push_back(val);
 		}
-		void get_neg(){
-			for(int i=0;i<neighbours.size();i++){
-				cout << neighbours[i] << " ";
-			}
-			cout << endl;
+		vector<int> get_neg(){
+			return neighbours;
 		}
 };
-
+int count_clauses=0;
+int count_vars=0;
 unordered_map<int,node> Graph;
 unordered_map<int,node> Graph_Dash;
 vector<vector<short int> > var_table; 
 string out;
-int count_clauses=0;
-int count_vars=0;
-
 
 void input(string filename){
 	int c1,c2;
@@ -54,6 +50,12 @@ void input(string filename){
 	if(file.is_open()){
 		file >> c1 >> c2;
 		do{
+			if(c1==0 && c2==0)
+			{
+
+				break;
+			}
+
 			if(Graph.find(c1)!=Graph.end()){
 				Graph[c1].set_outdegree();
 				// Graph[c2].indegree++;
@@ -81,7 +83,7 @@ void input(string filename){
 				}	
 				Graph[c1] = new1;
 			}
-		}while(c1!=0 && c2!=0)
+		}while(c1!=0 && c2!=0);
 
 		while(file.is_open()){
 			if(Graph_Dash.find(c1)!=Graph_Dash.end()){
@@ -128,13 +130,87 @@ void variable_matrix(){
 	for(int i=0;i<Graph_Dash.size();i++){
 		for(int j=0;j<Graph.size();j++){
 			if(Graph[i].get_indegree()<=Graph_Dash[j].get_indegree() || Graph[i].get_outdegree()<=Graph_Dash[j].get_outdegree()){
-				var_table[i][j]==1;
+				var_table[i][j]=1;
 				count_vars++;
-
 			}
 		}
 	}
 }
+
+void make_trivial_clauses(){
+	//this function makes clauses which force each node in the shorter graph to have exactly one mapping true
+	//Assumes that Graph_Dash is the smaller graph and Graph is the bigger graph
+	for(int i=0;i<Graph_Dash.size();i++)
+	{
+		for(int j=0;j<Graph.size();j++)
+		{
+			out+=(to_string((j+1) + i*Graph.size()) + " ");
+		}
+
+		out+="0\n";
+
+		for(int j=0;j<Graph_Dash.size();j++)
+		{
+			for(int k=0;k<Graph_Dash.size();k++)
+			{
+				if(j==k)
+					out+=(to_string((k+1 + (i*Graph.size()))) + " ");
+				else
+					out+=(to_string(-1*(k+1 + (i*Graph.size()) )) + " ");
+			}
+			out+="0\n";
+			count_clauses++;
+		}
+		count_clauses++;		
+	}
+}
+
+bool finder(vector<int> vec, int val)
+{
+	for(int i=0;i<vec.size();i++)
+	{
+		if(vec.at(i)==val)
+			return true;
+	}
+	return false;
+}
+
+
+
+void make_constrain_clauses()
+{
+	//this function makes clauses which enforce the constrain that a mapping is valid iff edges are present in both graphs
+	for(int i=0;i<Graph_Dash.size();i++)
+	{
+		for(int j=0;j<Graph.size();j++)
+		{
+			int v1 = j+1 + (i*Graph.size());
+			for(int k=0;k<Graph_Dash.size();k++)
+			{
+				for(int l=0;l<Graph.size();l++)
+				{
+					if(i==k || j==l)
+						continue;
+
+					int v2 = l+1+(k*Graph.size());
+
+					vector<int> neg_i = (Graph_Dash.at(i).get_neg());
+					vector<int> neg_k = (Graph_Dash.at(k).get_neg());
+					vector<int> neg_j = (Graph.at(j).get_neg());
+					vector<int> neg_l = Graph.at(l).get_neg();
+
+					if( (finder(neg_i, k) && !finder(neg_j,l)) || (!finder(neg_i, k) && finder(neg_j, l)) )
+					{
+						out += (to_string(-1*v1) + " " + to_string(-1*v2) + "0\n");
+					}
+
+				}
+			}
+		}
+
+	}
+}
+
 
 void make_clause_one(){
 	for(int i=0;i<Graph_Dash.size();i++){
@@ -160,30 +236,35 @@ void make_clause_two(){
 	}	
 }
 
-void make_clause_clash(){
-	vector<vector<int> > valid;
-	vector<int> short;
 
-	for(int i=0;i<Graph_Dash.size();i++){
-		short.clear();
-		for(int j=0;j<Graph.size();j++){
-			if(var_table[i][j]==1){
-				short.push_back((j+1) + i*Graph.size())
-			}
 
-		}
-		valid.push_back(short);
-	}
+// void make_clause_clash(){
+// 	vector<vector<int> > valid;
+// 	vector<int> short1;
 
-	for(int i=0;i<Graph_Dash.size();i++){
-		for(int j=0;j<valid[i].size()-1;j++){
-			for(int k=j+1;k<valid[i].size();k++){
-				out+= to_string(-1*(valid[i][k])) + " " + to_string(-1*(valid[i][k])) + " 0\n"; 
-				count_clauses++;
-			}
-		}
-	}
+// 	for(int i=0;i<Graph_Dash.size();i++){
+// 		short1.clear();
+// 		for(int j=0;j<Graph.size();j++){
+// 			if(var_table[i][j]==1){
+// 				short1.push_back((j+1) + i*Graph.size());
+// 			}
 
-}
+// 		}
+// 		valid.push_back(short1);
+// 	}
 
+// 	for(int i=0;i<Graph_Dash.size();i++){
+// 		for(int j=0;j<valid[i].size()-1;j++){
+// 			for(int k=j+1;k<valid[i].size();k++){
+// 				out+= to_string(-1*(valid[i][k])) + " " + to_string(-1*(valid[i][k])) + " 0\n"; 
+// 				count_clauses++;
+// 			}
+// 		}
+// 	}
+
+// }
+
+// int count_vars(){
+
+// }
 
